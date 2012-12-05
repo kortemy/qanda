e7cf626ac8965b4812209730b06fb25e96d269a7
Dusan Lilic <nob.venoda@gmail.com>
2012-12-05 10:25:49 +0100

    Cleanup.

--- qanda/bin/qanda/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns qanda.core-test
-  (:use clojure.test
-        qanda.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- qanda/bin/qanda/db/db_core.clj
+++
@@ -1,7 +0,0 @@
-(ns qanda.db.db-core)
-
-(defn db [] {:classname "com.mysql.jdbc.Driver"
-               :subprotocol "mysql"
-               :subname "//127.0.0.1:3306/qanda"
-               :user "root"
-               :password "root"})
